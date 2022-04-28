dc1fe65e8e4c9f3e1f7c0d5ac6ea10b71e4b80b2
Jonas Holbech <jh@jonasholbech.dk>
2022-04-28 22:39:17 +0200

    done

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import BasketContainer from "./components/BasketContainer";
 import styles from "./App.module.css";
 function App() {
   const [products, setProducts] = useState([]);
+  const [basket, setBasket] = useState([]);
   useEffect(() => {
     async function getProducts() {
       const res = await fetch("https://kea-alt-del.dk/t7/api/products");
@@ -16,8 +17,8 @@ function App() {
   return (
     <div className={styles.App}>
       <Nav />
-      <ProductList products={products} />
-      <BasketContainer />
+      <ProductList products={products} setBasket={setBasket} />
+      <BasketContainer setBasket={setBasket} basket={basket} />
     </div>
   );
 }
