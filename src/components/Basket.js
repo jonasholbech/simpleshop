5b7ad729faf297bda982d179455eac7b71e83849
Jonas Holbech <jh@jonasholbech.dk>
2022-04-28 22:52:11 +0200

    done

--- src/components/Basket.js
+++ src/components/Basket.js
@@ -1,9 +1,21 @@
-export default function basket() {
+export default function basket(props) {
+  const initialValue = 0;
+  const sumWithInitial = props.basket.reduce(
+    (previousValue, currentValue) => previousValue + currentValue.price,
+    initialValue
+  );
   return (
     <div>
       <ul>
-        <li>Stuff</li>
+        {props.basket.map((item) => {
+          return (
+            <li>
+              {item.productdisplayname} ${item.price}
+            </li>
+          );
+        })}
       </ul>
+      <p>Total: ${sumWithInitial}</p>
     </div>
   );
 }
