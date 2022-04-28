a9a9f2d8d0f11d60798ee396a52ec8b1be95ca58
Jonas Holbech <jh@jonasholbech.dk>
2022-04-28 22:32:41 +0200

    done

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
   return (
     <div className={styles.App}>
       <Nav />
-      <ProductList />
+      <ProductList products={products} />
       <BasketContainer />
     </div>
   );
