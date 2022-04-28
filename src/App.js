ca23a5fa79b49dcaa51a34ec7200d4f8e34afc17
Jonas Holbech <jh@jonasholbech.dk>
2022-04-28 22:22:46 +0200

    done

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import Nav from "./components/Nav";
 import ProductList from "./components/ProductList";
 import BasketContainer from "./components/BasketContainer";
+import styles from "./App.module.css";
 function App() {
   return (
-    <div className="App">
+    <div className={styles.App}>
       <Nav />
       <ProductList />
       <BasketContainer />
