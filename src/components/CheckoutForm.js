b0c9ad3ef554e7578f47d603bd7fcb6908dea6c5
Jonas Holbech <jh@jonasholbech.dk>
2022-04-28 23:17:35 +0200

    asdfgh

--- src/components/CheckoutForm.js
+++ src/components/CheckoutForm.js
@@ -1,8 +1,19 @@
+import { useRef } from "react";
 export default function CheckoutForm() {
+  const formEl = useRef(null);
+  function submitted(e) {
+    e.preventDefault();
+    console.log(formEl.current.elements.fullname.value);
+    console.log(formEl.current.elements.address.value);
+  }
   return (
     <div>
-      <form>
-        <input />
+      <form ref={formEl} onSubmit={submitted}>
+        <label htmlFor="fullname">Full name</label>
+        <input required type="text" name="fullname" id="fullname" />
+        <label htmlFor="address">Address</label>
+        <input required type="text" name="address" id="address" />
+        <button>Pay</button>
       </form>
     </div>
   );
